e2e42406a4ce0a954707623c5da697f6da034a13
Isaac S <8111@balcarras.gloucs.sch.uk>
2025-03-18 12:19:45 +0000

    initial commit

---
+++ Cbasics.c
@@ -0,0 +1,18 @@
+#include <stdio.h> //this is a header file library, i think they are like imports in python. this lets you run printf commantds
+
+
+// everything goes into a function called main, 
+int main() { 
+  printf("Hello World!"); //every c statement ends with a ;
+  int five = 5;
+  
+  five += 1;
+
+  printf (five);
+
+  // these end the function file thing as far as i cant tell, returning a zero means the file ran without error
+  return 0;
+}
+
+
+// find out why this does not work
